98b102bb024cf312a98f7628e629a104c517f2ed
Gigi Asatiani <123gigiasatiani123@gmail.com>
2023-09-01 18:04:14 +0400

    CSS-Grid + NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App" style={{ backgroundColor: "brown" }}>
-      <Button colorScheme="blue">Button</Button>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" " main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
   );
 }
 
