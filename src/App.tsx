cdf5d2342b4e5a0796ea220d35c1921a11ff0c7a
Gigi Asatiani <123gigiasatiani123@gmail.com>
2023-09-01 17:35:14 +0400

    installed chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App" style={{ backgroundColor: "brown" }}>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 }
 
 export default App;
