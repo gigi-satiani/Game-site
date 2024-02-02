6618cb4b6ee1f2b8884cfecba7779a2260b5eebd
Gigi Asatiani <123gigiasatiani123@gmail.com>
2024-02-02 17:41:03 +0400

    replaced load more button with infinite scroll. more badass

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,6 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { GameQuery } from "../App";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
@@ -22,20 +23,30 @@ const GameGrid = ({ gameQuery }: Props) => {
   } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7];
 
+  const fetchedGamesCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
     <Box padding="10px">
       {error && <Text>{error.message}</Text>}
-      <SimpleGrid columns={{ lg: 4, md: 3, sm: 1 }} spacing={6}>
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results.map((game) => (
-              <GameCard key={game.id} game={game} />
-            ))}
-          </React.Fragment>
-        ))}
-      </SimpleGrid>
+      <InfiniteScroll
+        dataLength={fetchedGamesCount}
+        hasMore={!!hasNextPage}
+        next={() => fetchNextPage()}
+        loader={<GameCardSkeleton />}
+      >
+        <SimpleGrid columns={{ lg: 4, md: 3, sm: 1 }} spacing={6}>
+          {isLoading &&
+            skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.results.map((game) => (
+                <GameCard key={game.id} game={game} />
+              ))}
+            </React.Fragment>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
       {hasNextPage && (
         <Button onClick={() => fetchNextPage()} marginTop={5}>
           {isFetchingNextPage ? "Loading..." : "Load More"}
